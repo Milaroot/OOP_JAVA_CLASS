11be3427a08537c48478aea704e9f75fa410edbe
Neil-Tu <61993786+Milaroot@users.noreply.github.com>
2022-10-25 00:19:34 +0800

    10.25

--- Java11/Class01.java
+++ Java11/Class01.java
@@ -1,32 +1,48 @@
 package Java11;
 
-abstract class Cshape {
+abstract class CShape {
     protected String color;
 
-    public void setColor(String str) {
+    public CShape(String str) {
         color = str;
     }
 
     public abstract void show();
 }
 
-class CCircle extends Cshape {
-    protected double radius;
+class CRectangle extends CShape {
+    int width, height;
+
+    public CRectangle(int w, int h) {
+        super("BLACK");
+        width = w;
+        height = h;
+    }
+
+    public void show() {
+        System.out.printf("color = %s, area = %d\n", color, width * height);
+    }
+}
+
+class CCircle extends CShape {
+    double radius;
 
     public CCircle(double r) {
+        super("BLUE");
         radius = r;
     }
 
     public void show() {
-        System.out.print("color=" + color + ".");
-        System.out.println("area=" + 3.14 * radius * radius);
+        System.out.printf("color = %s, area = %.2f\n", color, 3.14 * radius * radius);
     }
 }
 
 public class Class01 {
     public static void main(String[] args) {
-        CCircle myCircle = new CCircle(48763);
-        myCircle.setColor("blue&black");
-        myCircle.show();
+        CRectangle rec = new CRectangle(5, 10);
+        rec.show();
+
+        CCircle cir = new CCircle(2.0);
+        cir.show();
     }
 }
