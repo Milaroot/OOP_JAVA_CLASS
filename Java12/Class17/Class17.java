384b321ea2285b0fe37a626c1da77d11cb54441b
Neil-Tu <61993786+Milaroot@users.noreply.github.com>
2022-11-27 15:46:34 +0800

    update java12/class17

---
+++ Java12/Class17/Class17.java
@@ -0,0 +1,10 @@
+package Class17;
+
+public class Class17 {
+    public static void main(String arg[]) {
+        int tmp = Integer.parseInt("262904713") * 2;
+        int res = Short.parseShort("262904713");
+        System.out.println(tmp);
+        System.out.println(res); // Value out of range. Value:"262904713"
+    }
+}
